d3da5a42c05029d48eaaa41fcf51f7a98fc7b5bc
Yann B <ybycode@gmail.com>
2023-05-17 20:26:46 +0200

    Nix build from sources on github

--- default.nix
+++ default.nix
@@ -4,7 +4,11 @@ let
   myApp = pkgs.rustPlatform.buildRustPackage rec {
     name = "photor";
     version = "0.1.0";
-    src = pkgs.lib.cleanSource ./.;
+    src = builtins.fetchGit {
+      url = "https://github.com/ybycode/photor.git";
+      ref = "main";
+      # rev = "8f509d51d797106f245e53957c0419f3c0bc59ee";
+    };
 
     cargoSha256 = "sha256-nNwKDHJgjO4VFeIWAy6pB9+rFWr4JCsgC8Fu7g7nycA=";
     buildInputs = with pkgs; [ sqlite ];
