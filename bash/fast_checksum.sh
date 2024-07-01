dc5ef958a973773ee5200299f5a7a6f11f36a6e5
Yann B. <ybycode@gmail.com>
2024-07-01 09:32:55 +0200

    unsure about this one

--- bash/fast_checksum.sh
+++ bash/fast_checksum.sh
@@ -2,21 +2,36 @@
 
 set -euo pipefail
 
-if [[ $# -lt 1 ]]; then
-  >&2 echo "USAGE: $0 --help"
-  >&2 echo "USAGE: $0 FILE NBYTES"
-  exit 1
+help() {
+  >&2 echo "
+  This calculates the sha256sum of the size and first NBYTES of a given file
+
+  USAGE: $0 --help
+         $0 FILE NBYTES
+
+  EXAMPLES:
+
+    \$ $0 <(echo 123) 2
+    d4c2ea3c5f634412ee2da81444992bcf2ff866675fddc79a39a185480c635848
+
+    \$ $0 ./some/file 512
+    cae86a3ba19a5d86a95c79415be79b6551982d92c62a6d1c15926033f200ca8d"
+}
+
+if [[ $# = 1 ]] && [[ "$1" = "--help" ]]; then
+  help
+  exit 0
 fi
 
-if [[ "$1" = "--help" ]]; then
-  >&2 echo "help"
-  exit 0
+if [[ $# -ne 2 ]]; then
+  help
+  exit 1
 fi
 
 filename="$1"
 nbytes="$2"
 filesize="$(stat --printf="%s" "$filename")"
 
-cat <(echo -n "$filesize") <(head --bytes $nbytes "$filename") \
+cat <(echo -n "$filesize") <(head --bytes "$nbytes" "$filename") \
   | sha256sum \
   | awk '{ print $1 }'
