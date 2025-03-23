7cd915abe1a9bb702e50c11617b783f9193e7c20
Yann B. <ybycode@gmail.com>
2025-03-23 23:34:20 +0100

    Adds migrations to add archives related tables

---
+++ migrations/20250323212824_adds_archives.down.sql
@@ -0,0 +1,4 @@
+drop table if exists archives_items;
+drop table if exists archives_holders;
+drop table if exists archives;
+
---
+++ migrations/20250323212824_adds_archives.up.sql
@@ -0,0 +1,26 @@
+create table archives (
+  -- uuid stored as text
+  id text primary key not null,
+  media_type text not null,
+  inserted_at text not null
+);
+
+create table archives_holders (
+  id integer primary key not null,
+  archive_id text not null,
+  name text not null,
+  given_from text,
+  back_on text,
+
+  foreign key (archive_id) references archives (id)
+);
+
+create table archives_items (
+  id integer primary key not null,
+  archive_id text not null,
+  item_id integer not null,
+  inserted_at text not null,
+
+  foreign key (archive_id) references archives (id),
+  foreign key (item_id) references photos (id)
+);
