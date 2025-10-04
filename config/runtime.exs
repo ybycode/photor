ad739cc7e10bbf110060396a1776b3de19dd9fd8
Yann B. <ybycode@gmail.com>
2025-10-04 23:00:10 +0200

    runtime config for prod environment, to allow the socket to connect

--- config/runtime.exs
+++ config/runtime.exs
@@ -47,7 +47,7 @@ if config_env() == :prod do
   config :photor, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :photor, PhotorWeb.Endpoint,
-    url: [host: host, port: 4000, scheme: "http"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
